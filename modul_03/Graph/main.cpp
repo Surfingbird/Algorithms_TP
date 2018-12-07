f5bcc7be81832330fd2deb68c4230233ee168c81
a.baronskiy <a.baronskiy@mail.ru>
2018-12-07 20:11:57 +0300

    waste was deleted

--- modul_03/Graph/main.cpp
+++ modul_03/Graph/main.cpp
@@ -6,35 +6,5 @@
 #include "matrix_graph/matrix_graph.h"
 
 int main() {
-    SetGraph* graph = new SetGraph(4);
-    graph->AddEdge(1,2);
-    graph->AddEdge(1,2);
-    graph->AddEdge(1,2);
-
-    ArcGraph matrixGraph = ArcGraph(graph);
-
-    graph->Print();
-    std::cout << std::endl;
-    matrixGraph.Print();
-
-//  ListGraph graph(4);
-
-//  graph.AddEdge(0, 1);
-//  graph.AddEdge(0, 3);
-//  graph.AddEdge(2, 3);
-//  graph.AddEdge(2, 0);
-//  graph.AddEdge(1, 3);
-//  graph.AddEdge(3, 1);
-//
-//  std::cout << "Vertices count = " << graph.VerticesCount() << std::endl;
-//  std::vector<int> prev_vertices;
-//  graph.GetPrevVertices(1, prev_vertices);
-//  std::cout << "In edges for a vertex 1 = ";
-//  for (int prev : prev_vertices) {
-//    std::cout << "(" << prev << ", 1), ";
-//  }
-//  std::cout << std::endl;
-//
-//  graph.Print(std::cout);
   return 0;
 }
